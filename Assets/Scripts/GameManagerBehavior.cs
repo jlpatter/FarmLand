f8c38a7c0eb596f1de685d4f007a170478acf9ab
Joshua Patterson <jleegippies@gmail.com>
2021-06-13 11:04:53 -0500

    Slight balance changes

--- Assets/Scripts/GameManagerBehavior.cs
+++ Assets/Scripts/GameManagerBehavior.cs
@@ -10,8 +10,8 @@ public class GameManagerBehavior : MonoBehaviour {
         AllAnimals = new List<Tuple<GameObject, AnimalTypes>>();
         AnimalAttributesDict = new Dictionary<AnimalTypes, AnimalAttributes>();
         AnimalAttributesDict[AnimalTypes.Rabbit] = new AnimalAttributes(5.0f, 50.0f);
-        AnimalAttributesDict[AnimalTypes.Cow] = new AnimalAttributes(2.0f, 100.0f);
-        AnimalAttributesDict[AnimalTypes.Pig] = new AnimalAttributes(3.0f, 75.0f);
+        AnimalAttributesDict[AnimalTypes.Cow] = new AnimalAttributes(4.6f, 100.0f);
+        AnimalAttributesDict[AnimalTypes.Pig] = new AnimalAttributes(4.8f, 75.0f);
         // TODO: Add Chicken Here!
     }
 }
