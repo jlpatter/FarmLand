612b373cefb46620f535bc80de3b3a785f782dcb
Joshua Patterson <jleegippies@gmail.com>
2021-06-16 13:32:06 -0500

    Re-did the pause menu with text mesh pro

--- Assets/Scripts/PauseMenuButtons.cs
+++ Assets/Scripts/PauseMenuButtons.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuButtons : MonoBehaviour {
+
+    public void PushExitToMainMenuButton() {
+        SceneManager.LoadScene(0);
+    }
+    
     public void PushExitButton() {
         Application.Quit();
     }
