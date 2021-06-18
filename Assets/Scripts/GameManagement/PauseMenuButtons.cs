53c57ff4891eecc531746e3a8a7baec6d559bc16
Joshua Patterson <jleegippies@gmail.com>
2021-06-18 16:54:21 -0500

    Actually fixed that super annoying button highlighting bug

--- Assets/Scripts/GameManagement/PauseMenuButtons.cs
+++ Assets/Scripts/GameManagement/PauseMenuButtons.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
@@ -8,6 +9,13 @@ namespace GameManagement {
         public GameObject mainMenuButton;
 
         private void OnEnable() {
+            var coroutine = SelectButtonLater();
+            StartCoroutine(coroutine);
+        }
+        
+        private IEnumerator SelectButtonLater()
+        {
+            yield return null;
             EventSystem.current.SetSelectedGameObject(null);
             EventSystem.current.SetSelectedGameObject(mainMenuButton);
         }
